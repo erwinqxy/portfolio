ca789a6019cb502192975b7249be13fd021f3f40
Erwin Quek <72030222+erwinqxy@users.noreply.github.com>
2021-10-31 11:22:06 +0800

    Added animation to home page

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -4,10 +4,21 @@ import { Section, SectionText, SectionTitle } from '../../styles/GlobalComponent
 import Button from '../../styles/GlobalComponents/Button';
 import { LeftSection } from './HeroStyles';
 
+//to do 
 const Hero = (props) => (
-  <div>
-    Hero
-  </div>
+  <Section row nopadding>
+    <LeftSection>
+      <SectionTitle main center>
+        Welcome To <br />
+        My Personal Porfolio
+      </SectionTitle>
+      <SectionText>
+        I am a full stack developer with a passion for creating beautiful and
+        intuitive user experiences. 
+      </SectionText>
+      <Button onClick = {() => window.location = "https://drive.google.com/file/d/1tkgYEzQLdQT3SS5zU8kIHE1WEma58JAu/view?usp=sharing"}> Download Resume </Button>
+    </LeftSection>
+  </Section>
 );
 
 export default Hero;
