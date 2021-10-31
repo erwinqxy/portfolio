bb4eb773b2e33546bf8be36c3b574ab5ba2efc05
Erwin Quek <72030222+erwinqxy@users.noreply.github.com>
2021-10-31 12:18:44 +0800

    add tech stack

--- src/components/Technologies/Technologies.js
+++ src/components/Technologies/Technologies.js
@@ -1,12 +1,48 @@
 import React from 'react';
-import { DiFirebase, DiReact, DiZend } from 'react-icons/di';
+import { DiFirebase, DiPhotoshop, DiPython, DiReact, DiZend } from 'react-icons/di';
 import { Section, SectionDivider, SectionText, SectionTitle } from '../../styles/GlobalComponents';
 import { List, ListContainer, ListItem, ListParagraph, ListTitle } from './TechnologiesStyles';
 
 const Technologies = () =>  (
-  <div>
-    Technologies
-  </div>
+  <Section id="tech">
+    <SectionDivider />
+    <SectionTitle>Technologies</SectionTitle>
+    <SectionText>
+      I have experience with the following technologies:
+    <List>
+      <ListItem>
+        <DiReact size="5rem"/>
+        <ListContainer>
+          <ListTitle>Front-End</ListTitle>
+          <ListParagraph>
+            Experience with <br />
+            React.js 
+          </ListParagraph>        
+        </ListContainer>
+      </ListItem>
+      <ListItem>
+        <DiPython size="5rem"/>
+        <ListContainer>
+          <ListTitle>BackEnd-End</ListTitle>
+          <ListParagraph>
+            Experience with <br />
+            Python, Django <br />
+          </ListParagraph>        
+        </ListContainer>
+      </ListItem>
+      <ListItem>
+        <DiPhotoshop size="5rem"/>
+        <ListContainer>
+          <ListTitle>UI/UX</ListTitle>
+          <ListParagraph>
+            Experience with tools such as<br />
+            Figma, Miro <br />
+          </ListParagraph>        
+        </ListContainer>
+      </ListItem>
+    </List>
+    </SectionText>
+  </Section>
 );
 
 export default Technologies;
